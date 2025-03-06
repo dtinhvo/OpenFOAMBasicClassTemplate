fdd2fd345ac488f34791aa57e326ed8c83d2f6a8
Tinh Vo - dv59huli <tinh.vo@tu-darmstadt.de>
2025-03-06 14:25:49 +0100

    init

---
+++ lnInclude/templateClassNameREPLACEME.C
@@ -0,0 +1,1 @@
+../templateClassNameREPLACEME.C
---
+++ templateClassNameREPLACEME.C
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2013-2021 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "templateClassNameREPLACEME.H"
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(templateClassNameREPLACEME, 0);
+    defineRunTimeSelectionTable(templateClassNameREPLACEME, paramList);
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+namespace Foam
+{
+templateClassNameREPLACEME::templateClassNameREPLACEME
+( // contents of paramList  
+    const fvMesh& mesh
+)
+:
+    mesh(mesh)
+{
+}
+
+
+// * * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * //
+
+autoPtr<templateClassNameREPLACEME>
+templateClassNameREPLACEME::New
+( // contents of paramList  
+    const fvMesh& mesh
+)
+{
+    const word templateClassNameREPLACEMEType (word(
+        /*class selector location*/mesh.lookupObject<IOdictionary>("TBDREPLACEME").lookup("templateClassNameREPLACEME"))); 
+    Info<< "Selecting derived Class of ionicTransport: " << templateClassNameREPLACEMEType << endl;   
+    auto constructorIter = 
+        paramListConstructorTablePtr_->find(templateClassNameREPLACEMEType);                     
+    if (constructorIter == paramListConstructorTablePtr_->end())
+    {
+        FatalErrorInFunction
+            << "unknown baseType model " << templateClassNameREPLACEMEType << endl << endl
+            << "Valid schemes are :" << endl
+           << paramListConstructorTablePtr_->sortedToc()
+            << exit(FatalIOError);
+    }                                                                
+    return autoPtr<templateClassNameREPLACEME> ( 
+        constructorIter()
+        ( // contents of paramList   
+            mesh
+        ) 
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+templateClassNameREPLACEME::~templateClassNameREPLACEME()
+{}
+
+} // namespace Foam
+
+
+// ************************************************************************* //
